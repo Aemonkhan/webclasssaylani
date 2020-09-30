e7fe3166c7220b1c1a86f489a35ce4e77b15e612
jamhusnain <hasnainabdullah52@gmail.com>
2020-09-30 09:41:17 +0500

    some few changes

---
+++ indes.js
@@ -0,0 +1,17 @@
+
+
+// var name = "aemon"
+
+// console.log(`Good morning!
+// my name is ${name} 
+// // and i am bs.commerce student`)
+// var name = prompt("enter your name", 'name');
+// console.log(`${name}`);
+// var age = prompt(`please enter your age `);
+// if (age > 20) {
+//     console.log(`enter in classroom`)
+// }
+// else {
+//     console.log(`outside`)
+// }
+
